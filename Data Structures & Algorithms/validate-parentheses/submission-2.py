509d529c57941e6f6aa02ffc453e6171622872db
Hala sabobeh <76687709+hala-sabobeh@users.noreply.github.com>
2026-05-15 20:58:22 +0300

    Bulk sync: 4 submissions

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.py
@@ -0,0 +1,32 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        
+        for char in s:
+            #add to stack if opening brackets
+            if char == '(' or char == '{' or char == '[':
+                stack.append(char)
+            
+            #if it's closing check the opening for it from stack pop
+            elif char == ')':
+             
+                if stack[-1] == '(':
+                    stack.pop()  
+                else:
+                    return False #wrong mathcing
+            
+            elif char == '}':
+                
+                if stack[-1] == '{':
+                    stack.pop()
+                else:
+                    return False
+            
+            elif char == ']':
+                if stack[-1] == '[':
+                    stack.pop()
+                else:
+                    return False
+        
+        
+        return len(stack) == 0
